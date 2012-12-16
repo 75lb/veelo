0c7a0fe31befdcfcd26d7eac6d704e7c363325aa
Lloyd Brookes <lloyd.brookes@gmail.com>
2012-12-16 14:22:17 +0000

    first draft

--- compile-md.js
+++ compile-md.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node 
 
 var marked = require("marked"),
-	fs = require("fs");
+    fs = require("fs");
 	
-console.log("index.html", marked(fs.readFileSync("README.md", "utf-8")));
+var markup = marked(fs.readFileSync("README.md", "utf-8")),
+    indexHtml = fs.readFileSync("index-tmpl.html", "utf-8");
+
+console.log(indexHtml.replace(
+  '<article id="documentation"></article>', 
+  '<article id="documentation">' + markup + '</article>'
+));
