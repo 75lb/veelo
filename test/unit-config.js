a416c3e88891f2b61659618ade3ddcc18e3044ba
Lloyd Brookes <lloyd.brookes@gmail.com>
2012-12-22 15:36:51 +0000

    unit-config wip

---
+++ test/unit-config.js
@@ -0,0 +1,80 @@
+var assert = require("assert"),
+    Config = require("../lib/config");
+
+describe("Config", function(){
+    describe("definition: ", function(){
+        var config;
+    
+        beforeEach(function(){
+            config = new Config();
+        });
+    
+        it("should build config definition in groups", function(){
+            config.option("top", {type: "string", default: "root" });
+            config.group("veelo")
+                    .option("version", {type: "boolean"});
+            config.group("handbrake")
+                    .group("general")
+                        .option("update", { type: "boolean" });
+                   
+            assert.deepEqual(config.definition, {
+                top: {
+                    type: "string",
+                    default: "root"
+                },
+                veelo: {
+                   version: {
+                       type: "boolean"
+                   }
+                },
+                handbrake: {
+                   general: {
+                       update: {
+                           type: "boolean"
+                       }
+                   }
+                }
+            });
+            
+        });
+   
+        it("should set/get option", function(){
+            config.option("archiveDirectory", {type: "string"});
+            config.set("archiveDirectory", "testset");
+       
+            assert.equal(config.get("archiveDirectory"), "testset");
+        });
+    
+        it("should set/get option within specified group", function(){
+            config.group("veelo").option("archiveDirectory", {type: "string"});
+            config.group("veelo").set("archiveDirectory", "testset2");
+
+            assert.equal(config.group("veelo").get("archiveDirectory"), "testset");
+        });
+
+    });
+    
+    describe("defaults: ", function(){
+        it("should apply defaults", function(){
+            var config = new Config({
+                defaults: {
+                    test: "one", 
+                    group: {
+                        test: "two"
+                    }
+                }
+            });
+            
+            assert.equal(config.get("test"), "one");
+            assert.equal(config.group("group").get("test"), "two");
+        });
+        
+    });
+    
+    describe("validation: ", function(){
+        it("should validate string");
+        it("should validate number");
+        it("should validate regex");
+        it("should validate boolean");
+    });
+});
---
+++ test/unit-handbrakeCli.js
@@ -0,0 +1,173 @@
+var assert = require("assert"),
+    path = require("path"),
+    os = require("os"),
+    util = require("util"),
+    EventEmitter = require("events").EventEmitter,
+    Stream = require("stream"),
+    sinon = require("sinon"),
+    fs = require("fs-extra"),
+    Job = require("../lib/job"),
+    Config = require("../lib/config"),
+    HandbrakeCLI = require("../lib/handbrakeCli")
+    Veelo = require("../lib/veelo");
+
+var VIDEO1 = "clip1.mov", VIDEO1_M4V = "clip1.m4v",
+    VIDEO1_MKV = "clip1.mkv", VIDEO1_MP4 = "clip1.mp4",
+    VIDEO1_SRT = "clip1.srt",
+    VIDEO2 = "music.m4v", VIDEO2_M4V = "music_.m4v",
+    VIDEO2_MKV = "music.mkv", VIDEO2_MP4 = "music.mp4",
+    MEDIUM = "medium.m4v", MEDIUM_M4V = "medium_.m4v",
+    PRESET = "iPod", 
+    ASSETS_DIR = path.join(__dirname, "assets"),
+    FIXTURE_DIR = path.resolve(__dirname, "fixture"),
+    SUB_DIR = path.join(FIXTURE_DIR, "subdir"),
+    SUB_DIR2 = path.join(SUB_DIR, "subdir2"),
+    ORIGINALS_DIR = "veelo-originals";
+
+function log(msg){
+    console.log(util.inspect(msg, true, null, true));
+}
+
+function clearFixture(){
+    fs.removeSync(FIXTURE_DIR);
+    fs.mkdirsSync(SUB_DIR2);
+}
+
+function setupSingleFileFixture(file, done){
+    clearFixture();
+    fs.copy(path.join(ASSETS_DIR, file), path.join(FIXTURE_DIR, file), function(err){
+        if (err) throw err;
+        done();
+    });
+}
+
+describe("HandbrakeCLI", function(){
+    var handbrakeCLI, mockCp, handle;
+    
+    function ChildProcess(){
+        this.stdin = new ReadableStream();
+        this.stdout = new ReadableStream();
+        this.stderr = new ReadableStream();
+        this.kill = function(){
+            this.emit("exit", 0, "SIGTERM");
+        }
+    }
+    ChildProcess.prototype = new EventEmitter();
+
+    function ReadableStream(){
+        this.setEncoding = function(){};
+    }
+    ReadableStream.prototype = new Stream();
+    
+    beforeEach(function(){
+        mockCp = { 
+            spawn: function(){} 
+        };
+        handle = new ChildProcess();
+        sinon.stub(mockCp, "spawn").returns(handle);
+        handbrakeCLI = new HandbrakeCLI();
+        handbrakeCLI._inject({
+            cp: mockCp
+        });
+    });
+    
+    afterEach(function(){
+        mockCp.spawn.restore();
+    })
+    
+    it("should throw with no args", function(){
+        assert.throws(function(){
+            handbrakeCLI.spawn();
+        }, Error);
+    });
+    
+    it("should spawn with correct, passed in args", function(){
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }});
+        assert.ok(mockCp.spawn.args[0][1][0] == "-i", mockCp.spawn.args[0][1][0]);
+    });
+    
+    it("should fire 'handbrake-output' on handbrake stdout data", function(){
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }});
+        handbrakeCLI.on("handbrake-output", function(data){
+            assert.ok(data == "test data", data);
+        });
+        
+        handle.stdout.emit("data", "test data");
+    });
+
+    it("should NOT fire 'handbrake-output' on handbrake stderr data", function(){
+        var eventFired = false;
+        
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }, emitOutput: false });
+        handbrakeCLI.on("handbrake-output", function(data){
+            assert.ok(data == "test data", data);
+            eventFired = true;
+        });
+        handle.stderr.emit("data", "test data");
+        
+        assert.ok(eventFired == false);
+    });
+
+    it("SHOULD fire 'handbrake-output' on handbrake stderr data + emitOutput", function(){
+        var eventFired = false;
+
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }, emitOutput: true });
+        handbrakeCLI.on("handbrake-output", function(data){
+            assert.ok(data == "test data", data);
+            eventFired = true;
+        });
+        handle.stderr.emit("data", "test data");
+        
+        assert.ok(eventFired == true);
+    });
+    
+    
+    it("should fire 'terminated' on killing child_process", function(){
+        var eventFired = false;
+        
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }});
+        handbrakeCLI.on("terminated", function(){
+            eventFired = true;
+        })
+        handle.kill();
+        
+        assert.ok(eventFired == true);
+    });
+
+    it("should fire 'fail' on child_process exit with non-zero code", function(){
+        var eventFired = false;
+        
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }});
+        handbrakeCLI.on("fail", function(){
+            eventFired = true;
+        })
+        handle.emit("exit", 1);
+        
+        assert.ok(eventFired == true);
+    });
+
+    it("should fire 'fail' if Handbrake outputs 'no title found'", function(){
+        var eventFired = false;
+        
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }});
+        handbrakeCLI.on("fail", function(){
+            eventFired = true;
+        })
+        handle.stderr.emit("data", "No title found. Bitch.");
+        handle.emit("exit", 0);
+        
+        assert.ok(eventFired == true);
+    });
+    
+    it("should fire 'success' if Handbrake completes", function(){
+        var eventFired = false;
+        
+        handbrakeCLI.spawn({ handbrakeArgs: { i: "test.mov", o: "test.m4v" }});
+        handbrakeCLI.on("success", function(){
+            eventFired = true;
+        })
+        handle.emit("exit", 0);
+        
+        assert.ok(eventFired == true);
+    });
+});
