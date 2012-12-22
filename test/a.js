494cde1a033c7048aa67e63dc0976a477619628d
Lloyd Brookes <lloyd.brookes@gmail.com>
2012-12-22 23:29:25 +0000

    cli-args wip

--- test/a.js
+++ test/a.js
@@ -6,16 +6,15 @@ var optionDefinitions = {
     version: {type: "boolean"},
     "output-dir": { type: "string", alias: "o" },
     other: {type: "string"},
-    another: {type: "number"}
+    another: {type: "number"},
+    recurse: {type: "boolean"},
+    input: {type: "regex"}
 };
 
-var argv = process.argv = [ "node", "/usr/bin/blah", "file.js", "--version", "-o", "./testdir", "file2.mov" ];
+var argv = process.argv;
 argv.shift(); argv.shift();
 
 var output = {};
-
-l(argv);
-l("");
 
 _.each(optionDefinitions, function(def, option){
     if(argPassedIn(option, def.alias)){
@@ -30,66 +29,37 @@ function pickArgValue(option, alias, type){
     option = "--" + option;
     alias = alias ? "-" + alias : null;
     if (type == "boolean" ){
-        l("boolean: " + option + ", " + alias);
-        l(argv);
         argv = _.without(argv, option, alias);
-        l(argv);
-        l("");
         return true;
     } else {
         var argPos = argv.indexOf(option),
             argValuePos = argPos + 1;
             argValue = "";
         
-        if (argPos > 0){
+        if (argPos > -1){
             if (argValuePos < argv.length){
                 argValue = argv[argValuePos];
-                l("option found: " + option)
-                l(argv);
-                argv.splice(argPos, 2);
-                l(argv);
-                l("");
             }
+            argv.splice(argPos, argValue ? 2 : 1);
         }
         
         var aliasPos = argv.indexOf(alias),
             aliasValuePos = aliasPos + 1,
             aliasValue;
         
-        if (aliasPos > 0){
+        if (aliasPos > -1){
             if (aliasValuePos < argv.length){
-                l("alias found: " + alias);
                 aliasValue = argv[aliasValuePos];
-                l(argv);
-                argv.splice(aliasPos, 2);
-                l(argv);
-                l("");
             }
+            argv.splice(aliasPos, aliasValue ? 2 : 1);
         }
         
-        return argValue || aliasValue;
+        return argValue || aliasValue || "";
     }
 }
 
 function argPassedIn(arg, alias){
-    return argv.indexOf("--" + arg) > 0 || argv.indexOf("-" + alias) > 0;
+    arg = "--" + arg;
+    alias = "-" + alias;
+    return argv.indexOf(arg) > -1 || argv.indexOf(alias) > -1;
 }
-
-// a.shift(); a.shift(); 
-// 
-// a.forEach(function(arg){
-//     l(arg);
-//     l(getOption(arg));
-// });
-// 
-// function getOption(arg){
-//     if (optionDefinitions[arg] !== undefined){
-//         return arg;
-//     } else {
-//         _.each(optionDefinitions, function(val, option){
-//             if (val.alias === arg){
-//                 return key;
-//             }
-//         });
-//     }
-// }
