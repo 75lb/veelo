ed5f02f23d62d5fe5aa45227fcc1ec43c1a18d2b
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-02-14 21:29:35 +0000

    extracted handbrake-js

--- scripts/ubuntu-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [ "$(uname -n)"=="ubuntu" ]
-    then 
-    add-apt-repository --yes ppa:stebbins/handbrake-releases
-    apt-get update -qq
-    apt-get install -qq handbrake-cli
-fi
