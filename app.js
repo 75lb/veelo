57ab3ef5fccf8909d4c819d5c291bef45f532219
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-03-28 19:42:36 +0000

    moving to new work

--- app.js
+++ app.js
@@ -17,52 +17,21 @@ var command = process.argv.length == 0
 switch (command){
     default:
         veelo.encode(process.argv)
-            .on("queue-starting", function(queue){ 
-                if (queue.distinctExts){
-                    log("File types: " + _.map(queue.distinctExts(), function(value, key){
+            .on("monitor", function(job){
+                console.log("monitor");
+            })
+            .on("starting", function(job){
+                if (job.distinctExts){
+                    log("File types: " + _.map(job.distinctExts(), function(value, key){
                 		return util.format("%s(%d)", key, value);
                 	}).join(" "));
                 }
             })
-            .on("queue-complete", function(queue){ 
-                if (queue == this){
-                    log("Queue complete: " + queue.name); 
-                }
+            .on("complete", function(job){ 
+                log("Job complete: " + job.name); 
             })
-            .on("queue-info", function(queue, msg){ log(msg); })
-            // .on("job-starting", function(job){ log(job.name, "starting"); })
-            .on("job-progress", function(job, encode){ 
-                var full = "%s: %d\% complete [%d fps, %d average fps, eta: %s]\n",
-                    short = "%s: %d\% complete\n";
-                if(encode.fps){
-                    var line = util.format(full, job.data.file, encode.percentComplete, encode.fps, encode.avgFps, encode.eta);
-                    cursor.eraseLine(2);
-                    cursor.write(line);
-                    cursor.previousLine();
-                } else {
-                    var line = util.format(short, job.data.file, encode.percentComplete);
-                    cursor.eraseLine(2);
-                    cursor.write(line);
-                    cursor.previousLine();
-                }
-            })
-            // .on("job-complete", function(job){ log("Job complete: " + job.name); })
-            .on("job-success", function(job){ 
-                log(job.name, "success", job.data.file);
-            })
-            .on("job-fail", function(job){ 
-                log(job.name, "fail", job.data.file); 
-            })
-            .on("job-info", function(job, msg){ log(msg); })
-            .on("job-warning", function(job, msg){ log(msg); })
-            .on("job-error", function(job, err){ 
-                log(err.message, "error");
-            })
-            .on("job-terminated", function(job){ log("job-terminated: " + job.name); })
-            .on("error", function(err){ 
-                console.log(err); 
-                veelo.help(process.argv, log);
-            });
+            .on("progress", console.log)
+            .on("info", console.log );
         break;
     case "info":
         veelo.info(process.argv)
