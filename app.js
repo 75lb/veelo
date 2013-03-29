403a15770e2f837ca2935dfd6794a9e32ba64874
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-03-29 15:30:58 +0000

    monitoring wip

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ switch (command){
             .on("monitor", function(job){
                 cursor.eraseData(2);
                 cursor.goto(1, 1);
-                this.print();
+                print(this);
             });
         break;
     case "info":
@@ -82,4 +82,34 @@ function log(msg, type, file){
             .write(util.format(" [%s]\n", file))
             .reset();
     }
+}
+
+function print(job, indent){
+    indent = indent || 0;
+    var padding = "";
+    for(var i=0; i<indent; i++){
+        padding += " ";
+    }
+    cursor.write(padding)
+        .bold().write(job.name).reset();
+    
+    if (job.runOn) cursor.write(" on-" + job.runOn);
+    if (job.progress) cursor.write(" " + job.progress.percentComplete + "%");
+    if (job.running) cursor.fg.green(); else cursor.fg.red();
+    cursor.write(" running").fg.reset()
+    if (job.successful) cursor.fg.green(); else cursor.fg.red();
+    cursor.write(" successful").fg.reset()
+    if (job.complete) cursor.fg.green(); else cursor.fg.red();
+    cursor.write(" complete").fg.reset()
+    if (job.terminated) cursor.fg.green(); else cursor.fg.red();
+    cursor.write(" terminated").fg.reset()
+    if (job.ignored) cursor.fg.green(); else cursor.fg.red();
+    cursor.write(" ignored").fg.reset()
+    cursor.write(" " + job.errors)
+    cursor.write("\n");
+        
+    // l("%s%s [rO: %s, p: %s, r: %s, s: %s, c: %s, e: %s, t: %s, i: %s]", padding, this.name, this.runOn || "", this.progress ? this.progress.percentComplete : "", this.running, this.successful, this.complete, this.errors, this.terminated, this.ignored);
+    job.children.forEach(function(child){
+        print(child, indent + 2);
+    });
 }
