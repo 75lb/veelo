3d85a3fe5a80638234ef79ec27f58744c5b541ef
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-10 15:53:03 +0200

    rewrite wip.. failing test

---
+++ test/commands.js
@@ -0,0 +1,22 @@
+var test = require("tape"),
+    encode = require("../lib/encode");
+
+test("simple", function(t){
+    var files = [
+        "movies/one.mp4",
+        "movies/folder/three.mp4",
+        "five.avi"
+    ];
+    var options = {
+        preset: "Normal",
+        optimize: true,
+        ext: "mkv"
+    };
+    var commands = encode.buildCommands(files, options);
+    t.deepEqual(commands, [
+        { input: "movies/one.mp4", output: "movies/one.mkv", preset: "Normal", optimize: true },
+        { input: "movies/folder/three.mp4", output: "movies/folder/three.mkv", preset: "Normal", optimize: true },
+        { input: "five.avi", output: "five.mkv", preset: "Normal", optimize: true },
+    ]);
+    t.end();
+});
